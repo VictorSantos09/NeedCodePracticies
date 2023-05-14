978ca6c43724349d67d0798c44cf62fb44b6cb7e
Victor Santos <victorluizsantos49@gmail.com>
2023-05-13 23:24:14 -0300

    inicio

---
+++ NeetCode/Blind75/ArraysAndHashing/ContainsDuplicate.cs
@@ -0,0 +1,55 @@
+﻿namespace NeetCode.Blind75.ArraysAndHashing
+{
+    //Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct
+    public class ContainsDuplicate
+    {
+        private static int[] nums = new int[10];
+        private static int counter = 0;
+        private static Random Random = new Random();
+
+        public static bool HardWay()
+        {
+            PopulateNums();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int number = nums[i];
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (counter >= 2)
+                        return true;
+
+                    else if (nums[j].Equals(number))
+                        counter++;
+                }
+
+                counter = 0;
+            }
+            return false;
+        }
+
+        public static bool EasyWay()
+        {
+            PopulateNums();
+
+            Console.WriteLine("\n");
+
+            var numsList = nums.ToList();
+            foreach (var n in numsList)
+            {
+                if (numsList.FindAll(x => x.Equals(n)).Count >= 2)
+                    return true;
+            }
+            return false;
+        }
+
+        private static void PopulateNums()
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = Random.Next(5);
+                Console.Write($"{nums[i]} ");
+            }
+        }
+    }
+}
---
+++ NeetCode/Blind75/ArraysAndHashing/ValidAnagram.cs
@@ -0,0 +1,77 @@
+﻿namespace NeetCode.Blind75.ArraysAndHashing;
+
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+public class ValidAnagram
+{
+    public static void HardWay(string s, string t)
+    {
+        if (t.Length != s.Length)
+            Console.WriteLine($"{t} não é um anagrama");
+
+        else
+        {
+            var sList = s.ToList();
+            List<Detail> data = new List<Detail>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (data.Find(x => x.Letter == s[i]) is null)
+                {
+                    var letter = s[i];
+                    int count = sList.FindAll(x => x.Equals(s[i])).Count;
+                    Detail detail = new Detail(letter, count);
+                    data.Add(detail);
+
+                    if (t.ToList().FindAll(x => x.Equals(letter)).Count != count)
+                    {
+                        Console.WriteLine($"{t} não é um anagrama");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    public static bool EasyWay(string s, string t)
+    {
+        if (t.Length != s.Length)
+            return false;
+
+        else
+        {
+            Dictionary<char, int> values = new Dictionary<char, int>();
+
+            for (int i = 0; i < t.Length; i++)
+            {
+                char letter = s[i];
+
+                if (t.Contains(letter) == false)
+                    return false;
+
+                if (values.ContainsKey(letter) == false)
+                    values.Add(letter, 1);
+
+                else
+                {
+                    values.TryGetValue(letter, out int count);
+                    count++;
+                    values[letter] = count;
+                }
+            }
+            return true;
+        }
+    }
+}
+
+public class Detail
+{
+    public char Letter { get; set; }
+    public int Count { get; set; }
+
+    public Detail(char letter, int count)
+    {
+        Letter = letter;
+        Count = count;
+    }
+}
