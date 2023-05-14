d3adf713da2292f99088446ccea2af5c09ba6f7d
Victor Santos <victorluizsantos49@gmail.com>
2023-05-14 11:55:29 -0300

    TwoSum added

---
+++ NeetCode/Blind75/ArraysAndHashing/TwoSum.cs
@@ -0,0 +1,37 @@
+﻿namespace NeetCode.Blind75.ArraysAndHashing
+{
+    internal class TwoSum
+    {
+        public static bool Solution(int[] nums, int target)
+        {
+            //Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+            //You may assume that each input would have exactly one solution, and you may not use the same element twice.
+            //You can return the answer in any order.
+
+            var filtered = nums.Where(x => x < target).ToArray();
+            if (filtered.Length == 0)
+                return false;
+
+            List<string> output = new List<string>();
+
+            for (int i = 0; i < filtered.Length; i++)
+            {
+                for (int j = i + 1; j < filtered.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        string text = $"[{i},{j}] {nums[i]} + {nums[j]} == {target}";
+                        output.Add(text);
+                    }
+                }
+            }
+            
+            foreach (var i in output)
+            {
+                Console.WriteLine(i);
+            
+            }
+            return true;
+        }
+    }
+}
--- NeetCode/Program.cs
+++ NeetCode/Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿using NeetCode.Blind75.ArraysAndHashing;
+
+int[] nums = { 3, 2, 4, 7, 8, 5, 4, 6, 3, 1, 2, 4, 3 };
+int target = 9;
+TwoSum.Solution(nums, target);
